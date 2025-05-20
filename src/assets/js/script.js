451790bcddac97a882d3ba5b63b2a2261852903a
Emmity8 <juliocesar.vieirax@gmail.com>
2025-05-20 11:49:34 -0300

    Subindo arquivos para o repositório

---
+++ src/assets/js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded',()=>{
+//DECLARANDO AS VARIÁVEIS USANDO DOM
+const loginForm=document.getElementById('form-login');
+const mensagem=document.getElementById('message');
+const video=document.querySelector('.video-container');
+const loginContainer=document.getElementById('Login-container')
+const buttonEntrar=document.getElementById('btnEntrar')
+const buttonSair=document.getElementById('btnSair')
+
+//VERIFICA SE O USUÁRIO ESTÁ LOGADO NO LOCALSTORAGE
+
+
+})
